f7da47aba91207177905b69bdbd7b8e8944fc7a7
Guilherme Araujo <guiaszul@gmail.com>
2023-10-31 14:23:57 -0300

    Add functions de exemplo com o tipo enum

--- example.exs
+++ example.exs
@@ -1,1 +1,14 @@
 IO.puts("Hello world from Elixir");
+
+# Enum.sum/1 for numeric values
+ 6 == Enum.sum([1, 2, 3])
+ 6 == Enum.sum(1..3)
+ 
+ # reduce to extract/transform a value during sum
+ 6 = Enum.reduce(%{ a: 1, b: 2, c: 3 }, 0, fn({_k, v}, acc) -> v + acc end)
+
+ [2, 4, 6] = Enum.map([1, 2, 3], fn(i) -> i * 2 end)
+
+
+# map to a keyword list
+[a: 2, a: 4, a: 6] = Enum.map([1, 2, 3], fn(i) -> {:a , i * 2} end)
